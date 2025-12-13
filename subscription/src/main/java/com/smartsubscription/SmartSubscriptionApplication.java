adb1a37286c10fc797c480a1dad3f38ef02153e7
ShivaSirikonda <shivasirikonda6@gmail.com>
2025-12-13 21:46:25 +0530

    set up the project

---
+++ subscription/src/main/java/com/smartsubscription/SmartSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.smartsubscription;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartSubscriptionApplication.class, args);
+    }
+}
